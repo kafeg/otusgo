3e51c2cf1461ccbdda0ed8c077ea9f1b0698c9dd
Vitaly Petrov <v31337@gmail.com>
2020-11-10 17:16:22 +0300

    hw01

--- hw01.go
+++ hw01.go
@@ -41,4 +41,150 @@ func main() {
 	printCurrentTime(time2)
 
 	fmt.Printf("Offset: %v\n", response.ClockOffset)
+
+	// --- just random basic samples from the lection
+
+	// vars
+	{
+		fmt.Printf("Vars\n")
+		var a int = 5 // var a with type 'int'
+		var b = 6 // var b with auto-type from value
+		c := 7 // var c with auto-type from value and with operator := instead of 'var'
+		fmt.Printf("%d, %d, %d\n", a, b, c)
+
+		c, d := 9, 132 // new value for c and new var d
+		b = 15
+		fmt.Printf("%d, %d, %d, %d\n", a, b, c, d)
+	}
+
+	// constants
+	{
+		fmt.Printf("Constants\n")
+		const (
+			a = 1 // a == 1
+			b = 2 // b == 2
+			c    // c == 2
+			d    // d == 2
+		)
+		fmt.Printf("%d %d %d %d\n", a, b, c, d)
+
+	}
+
+	// constants and iota, iota is special incrementer
+	{
+		fmt.Printf("Constants iota\n")
+		const (
+			a = iota // a == 0
+			b = iota // b == 1
+			c = iota // c == 2
+			d = iota * 2 // here iota should be 3 and we multiplies it on 2
+			e // e == 8 (implicitely e = iota * 2)
+		)
+		fmt.Printf("%d %d %d %d %d\n", a, b, c, d, e)
+	}
+
+	// 'if' block
+	{
+		fmt.Printf("Conditions\n")
+		if response.ClockOffset < 1 {
+			fmt.Printf("Offset less than 1 second!\n")
+		} else {
+			fmt.Printf("Offset more than 1 second!\n")
+		}
+
+		//define global a, b and some stupid usage to prevent 'unused' err
+		a, b := 1, 2
+		a = b
+		b = a
+
+		//here a, b is in local scope for the 'if' block, vars above ignored
+		if a, b := 5, 7; a > b {
+			println("yes")
+		} else {
+			println("no")
+		}
+	}
+
+	// cycles
+	{
+		fmt.Printf("Cycles\n")
+		//simple for
+		for i := 0; i < 10; i++ {
+			println(i)
+		}
+
+		//while
+		i := 0
+		for i < 10 {
+			println(i)
+			i++
+		}
+
+		//infinity cycle
+		i = 0 //use i from above
+		for {
+			println(i)
+			i++
+			if i > 10 {
+				break
+			}
+		}
+	}
+
+	// iterate over array with operator 'range', we have i - index and v - value
+	{
+		fmt.Printf("Range array\n")
+		a := [...]int{81, 54, 43, 66} // array can't be increased, it's read-only type, but can be copied
+		for i, v := range a {
+			println(i, v)
+		}
+	}
+
+	//switches
+	{
+		fmt.Printf("Switches\n")
+
+		//simple, like with 'break' on each  case
+		a := 15
+		switch a {
+		case 10:
+			println("One")
+		case 15:
+			println("Two")
+		case 30:
+			println("Three")
+		default:
+			println("DEFAULT!")
+		}
+
+		//like without 'break' on each step, 'fallthrough' used for it (inversed break logic)
+		switch a {
+		case 10:
+			println("One")
+			fallthrough
+		case 15:
+			println("Two")
+			fallthrough
+		case 30:
+			println("Three")
+			fallthrough
+		default
+			:println("DEFAULT!")
+		}
+
+		//conditional switch
+		a = 10
+		switch {
+		case a < 10:
+			println("One")
+		case a < 30 && a >= 10:
+			println("Two")
+		case a > 3:
+			println("Three")
+		default:
+			println("DEFAULT!")
+		}
+	}
+
+
 }
